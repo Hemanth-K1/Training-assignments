1578ec4966e462140cbdf2e3e94fcb0021101d6e
Hemanth K <hemanthhemik2003@gmail.com>
2024-10-23 10:17:02 +0530

    File is added by hemanth

---
+++ 21-10-24/21-10-24.py
@@ -0,0 +1,11 @@
+def count_character(d):
+    l = len(d)
+    d1 = {}
+    for i in d:
+        if i in d1:
+            d1[i] = d1[i] + 1 
+        else:
+            d1[i] = 1 
+        return d1 
+d = "skywavessoftwares"
+print(count_character(d))
